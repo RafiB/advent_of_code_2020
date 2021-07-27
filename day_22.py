e026da075db307b0eb2ff85356d1bd289de06f48
Rafi Blecher <rafiblecher@gmail.com>
2021-07-27 18:48:23 +1000

    solve day 22 star 2

--- day_22.py
+++ day_22.py
@@ -86,27 +86,46 @@ def solve(starting_decks):
 
         current_deck.insert(0, int(line))
 
-    # play
-    while player2_deck and player1_deck:
-        player1_card = player1_deck.pop()
-        player2_card = player2_deck.pop()
-
-        if player1_card > player2_card:
-            player1_deck.insert(0, player1_card)
-            player1_deck.insert(0, player2_card)
-        else:
-            player2_deck.insert(0, player2_card)
-            player2_deck.insert(0, player1_card)
-
-    print(f"{player1_deck=} {player2_deck=}")
-
-    winning_deck = player1_deck or player2_deck
+    winning_deck = play(player1_deck, player2_deck)
 
     score = 0
     for i, card in enumerate(winning_deck):
         score += (i+1) * card
     return score
 
+def play(player1_deck, player2_deck):
+    previous_decks = tuple()
+
+    while player1_deck and player2_deck:
+        game_state = (tuple(player1_deck), tuple(player2_deck))
+        if game_state in previous_decks:
+            """
+            if there was a previous round in this game that had exactly the same cards in the same order
+            in the same players' decks, the game instantly ends in a win for player 1.
+            """
+            return player1_deck
+
+        previous_decks = previous_decks + (game_state,)
+
+        player1_card = player1_deck.pop()
+        player2_card = player2_deck.pop()
+
+        if len(player1_deck) >= player1_card and len(player2_deck) >= player2_card:
+            p1_sub_deck = player1_deck[-player1_card:]
+            p2_sub_deck = player2_deck[-player2_card:]
+            winning_deck = play(
+                p1_sub_deck,
+                p2_sub_deck,
+            )
+            winning_deck = player1_deck if winning_deck is p1_sub_deck else player2_deck
+        else:
+            winning_deck = player1_deck if player1_card > player2_card else player2_deck
+
+        winning_deck.insert(0, player1_card if winning_deck is player1_deck else player2_card)
+        winning_deck.insert(0, player2_card if winning_deck is player1_deck else player1_card)
+
+    return player1_deck or player2_deck
+
 if __name__ == "__main__":
-    assert solve(TEST_INPUT) == 306
+    assert solve(TEST_INPUT) == 291
     print(solve(PUZZLE_INPUT))
